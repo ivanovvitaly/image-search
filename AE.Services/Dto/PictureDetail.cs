bcb2b6bd4a2545018decc1511ff6065279b324ef
Vitaly Ivanov <vitaly.ivanov@agileengine.com>
2020-12-29 20:12:34 +0200

    picture base class

--- AE.Services/Dto/PictureDetail.cs
+++ AE.Services/Dto/PictureDetail.cs
@@ -2,18 +2,13 @@ using System.Text.Json.Serialization;
 
 namespace AE.Services.Dto
 {
-    public class PictureDetail
+    public class PictureDetail : Picture
     {
-        public string Id { get; set; }
-
         public string Author { get; set; }
 
         public string Camera { get; set; }
         
         public string Tags { get; set; }
-
-        [JsonPropertyName("cropped_picture")]
-        public string CroppedPicture { get; set; }
 
         [JsonPropertyName("full_picture")]
         public string FullPicture { get; set; }
